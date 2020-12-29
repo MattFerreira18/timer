8a8b51dcae3f6f66aef89639ea07b609dbc99e82
Matheus <matheusgustomf@gmail.com>
2020-12-29 08:02:31 -0300

    add editorconfig to app

--- scripts/stopWatch.js
+++ scripts/stopWatch.js
@@ -6,74 +6,74 @@ let timer;
 
 document.addEventListener('click', (event) => {
 
-    if (event.target === playPause) initOrPause();
-    if (event.target === reset) resetTime();
+  if (event.target === playPause) initOrPause();
+  if (event.target === reset) resetTime();
 
 });
 
 window.addEventListener('keydown', (keyEvent) => {
 
-    if (keyEvent.code === 'Space') return initOrPause();
+  if (keyEvent.code === 'Space') return initOrPause();
 
 });
 
 function formatTime(seconds) {
 
-    const date = new Date(seconds * 1000);
-    return date.toLocaleTimeString('pt-br', { hour12: false, timeZone: 'UTC' });
+  const date = new Date(seconds * 1000);
+  return date.toLocaleTimeString('pt-br', { hour12: false, timeZone: 'UTC' });
 
 };
 
 function initOrPause() {
 
-    if (playPause.textContent === 'Play') {
+  if (playPause.textContent === 'Play') {
 
-        setTimeout(() => {
+    setTimeout(() => {
 
-            playPause.textContent = 'Pause';
-            playPause.classList.remove('play');
-            playPause.classList.add('pause');
+      playPause.textContent = 'Pause';
+      playPause.classList.remove('play');
+      playPause.classList.add('pause');
 
-            timeNumbers.classList.remove('paused');
-            timeNumbers.classList.add('on');
+      timeNumbers.classList.remove('paused');
+      timeNumbers.classList.add('on');
 
-            timer = setInterval(() => {
-                seconds++;
-                timeNumbers.innerHTML = formatTime(seconds);
+      timer = setInterval(() => {
+        seconds++;
+        timeNumbers.innerHTML = formatTime(seconds);
 
-            }, 1000);
+      }, 1000);
 
-        }, 600);
+    }, 600);
 
-    } else {
+  } else {
 
-        setTimeout(() => {
+    setTimeout(() => {
 
-            playPause.textContent = 'Play';
-            playPause.classList.remove('pause');
-            playPause.classList.add('play');
+      playPause.textContent = 'Play';
+      playPause.classList.remove('pause');
+      playPause.classList.add('play');
 
-            timeNumbers.classList.remove('on');
-            timeNumbers.classList.add('paused');
-            clearInterval(timer);
+      timeNumbers.classList.remove('on');
+      timeNumbers.classList.add('paused');
+      clearInterval(timer);
 
-        }, 600);
+    }, 600);
 
-    };
+  };
 
 };
 
 function resetTime() {
 
-    playPause.textContent = 'Play';
-    playPause.classList.remove('pause');
-    playPause.classList.add('play');
+  playPause.textContent = 'Play';
+  playPause.classList.remove('pause');
+  playPause.classList.add('play');
 
-    timeNumbers.classList.remove('paused');
-    timeNumbers.classList.add('on');
+  timeNumbers.classList.remove('paused');
+  timeNumbers.classList.add('on');
 
-    clearInterval(timer);
-    timeNumbers.innerHTML = '00:00:00';
-    seconds = 0;
+  clearInterval(timer);
+  timeNumbers.innerHTML = '00:00:00';
+  seconds = 0;
 
 };
